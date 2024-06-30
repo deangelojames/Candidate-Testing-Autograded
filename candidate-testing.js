4767c446903f93d188a8617e837277f7ed55a30e
De'Angelo Bennett <bennett.deangelo@gmail.com>
2024-06-30 11:52:18 -0500

    Adding completed assingment 1 to file.

--- candidate-testing.js
+++ candidate-testing.js
@@ -13,64 +13,77 @@ let candidateAnswer = "";
 //TODO: Variables for Part 2
 let questions = ["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
 let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
-let candidateAnswers = "";
-
+let candidateAnswers = [];
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-  const input = require('readline-sync');
 
-  let candidateName = input.question("Please enter candidate Name: ");
+  candidateName = input.question("Please enter candidate name: ");
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
 
-  const input = require('readline-sync');
-
   for (let i = 0; i < questions.length; i++) {
-    let candidateAnswers = input.question(questions[i]);
+    responses = input.question(`${i + 1}) ${questions[i]}`);
+    candidateAnswers.push(responses);
   }
+  return candidateAnswers;
 }
+
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
+  console.log(`Candidate Name: ${candidateName}`);
+  for (let i = 0; i < questions.length; i++) {
+    console.log(`${i + 1}) ${questions[i]}`);
+    console.log(`Your Answer: ${candidateAnswers[i]}`);
+  
+    if (candidateAnswers[i].toLowerCase() === correctAnswers[i].toLowerCase()) {
+      console.log(`Correct Answer: ${correctAnswers[i]}`);
+    } else {
+      console.log(`You answered incorrectly, the correct answer is ${correctAnswers[i]}.`);
+    }
+  }
+    let numberOfCorrectAnswers = 0;
 
-console.log("Your Answer: " + candidateAnswer );
-console.log("Correct Answer: " + correctAnswer );
+    for (let i = 0; i < correctAnswers.length; i++) {
+      if (candidateAnswers[i].toLowerCase() === correctAnswers[i].toLowerCase()) {
+        numberOfCorrectAnswers++;
+      }
+    }
 
-if (candidateAnswer === correctAnswer) {
-    console.log("You answered correctly.");
-} else {
-    console.log("You answered incorrectly.");
+    let numberOfQuestions = questions.length;
+    let grade = (numberOfCorrectAnswers / numberOfQuestions) * 100;  //TODO 3.2 use this variable to calculate the candidates score.
+
+    console.log(`>>> Overall grade: ${grade}% (${numberOfCorrectAnswers} of ${numberOfQuestions}) <<<`);
+
+    if (grade >= 80) {
+      console.log(`>>> Status: PASSED <<<`);
+    } else {
+      console.log(`>>> Status: FAILED <<<`);
+    }
+    return grade;
 }
 
+  function runProgram() {
+    askForName();
+    // TODO 1.1c: Greet candidate using their name //
+    console.log(`Hello, thanks for taking the test ${candidateName}.`);
+    askQuestion(this.questions);
+    gradeQuiz(this.candidateAnswers);
+  }
 
-
-  let grade;  //TODO 3.2 use this variable to calculate the candidates score.
-
-
-  return grade;
-}
-
-function runProgram() {
-  askForName();
-  // TODO 1.1c: Greet candidate using their name //
-   console.log("Hello, thanks for taking the test, " + candidateName + '.' );
-  askQuestion();
-  gradeQuiz(this.candidateAnswers);
-}
-
-// ----------- Don't write any code or change any code below this line ---------- //
-module.exports = {
-  candidateName: candidateName,
-  question: question,
-  correctAnswer: correctAnswer,
-  candidateAnswer: candidateAnswer,
-  questions: questions,
-  correctAnswers: correctAnswers,
-  candidateAnswers: candidateAnswers,
-  gradeQuiz: gradeQuiz,
-  runProgram: runProgram
-};
+  // ----------- Don't write any code or change any code below this line ---------- //
+  module.exports = {
+    candidateName: candidateName,
+    question: question,
+    correctAnswer: correctAnswer,
+    candidateAnswer: candidateAnswer,
+    questions: questions,
+    correctAnswers: correctAnswers,
+    candidateAnswers: candidateAnswers,
+    gradeQuiz: gradeQuiz,
+    runProgram: runProgram
+  };
